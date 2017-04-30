fc93f7391175410e21862067850fe787291e249b
wcss <wcss@DESKTOP-I5H6FJE>
2017-04-30 17:27:30 +0800

    初始导入

---
+++ src/main/java/com/boco/dal/gitlab/statistics/GitlabstatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.boco.dal.gitlab.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabstatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabstatisticsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/boco/dal/gitlab/statistics/GitlabstatisticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boco.dal.gitlab.statistics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitlabstatisticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
